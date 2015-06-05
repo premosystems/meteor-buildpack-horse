49a8776074311d8b9f7dcd8d2179d85435e68473
Vincil Bishop <vincil.bishop@vbishop.com>
2015-06-05 08:13:19 -0500

    Updated for build pack changes

--- extra/meteor_path.sh
+++ extra/meteor_path.sh
@@ -4,5 +4,5 @@ echo "-----> Adding $PATH:$HOME/.meteor env"
 cat > "$APP_CHECKOUT_DIR"/.profile.d/meteor_path.sh <<EOF
   #!/bin/sh
   # Set MONGO_URL to MONGOHQ_URL if it's not already set
-  export PATH=\$PATH:$HOME/.meteor
+  export PATH=$PATH:$HOME/.meteor
 EOF
